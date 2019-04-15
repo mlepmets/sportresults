c881a42f727b29522a02e5d88fe62d895305dea2
Martti Lepmets <mlepmets@gmail.com>
2019-04-15 12:55:08 +0300

    fixed untracked file

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  htmlWhitespaceSensitivity: 'strict',
-  semi: true,
-  tabWidth: 2,
-  useTabs: false,
-  bracketSpacing: true,
-  arrowParens: 'avoid'
-};
