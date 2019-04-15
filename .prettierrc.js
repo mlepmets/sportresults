681e9a065fef74fb48afabc94005031383028de9
Martti Lepmets <mlepmets@gmail.com>
2019-04-15 12:50:46 +0300

    test

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  htmlWhitespaceSensitivity: 'strict',
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid'
+};
